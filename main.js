b778be4b85c1b0dd69aee05d23bf95ee3bd4b150
Marchioro <olikujy03@gmail.com>
2024-06-17 11:44:29 -0300

    Entrega

---
+++ main.js
@@ -0,0 +1,12 @@
+let nome = prompt("Qual é seu nome?");
+let idade = prompt("E a sua idade?");
+idade = parseInt(idade);
+let anoNasc= 2024 - idade;
+
+alert(`Seu nome é ${nome} e sua idade é ${idade} 
+    
+         Voce nasceu em ${anoNasc}
+         Voce tem ${idade * 12} meses de vida e ${idade * 365 + 5} dias de vida!
+         Parabens!! 
+
+    `);leo
